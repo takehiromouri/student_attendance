7bd68be4cb544f4b8a5e16947d4f75803eb9a539
Takehiro <takehiromouri@gmail.com>
2016-12-02 14:37:06 +0545

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module StudentAttendance
+  class Application < Rails::Application
+    config.generators do |g|
+		  g.helper false
+		  g.assets false
+		end
+  end
+end
