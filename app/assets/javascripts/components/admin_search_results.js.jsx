b158707f2784108ae4f314ef53426a1214052738
Takehiro <takehiromouri@gmail.com>
2016-12-09 00:06:15 +0545

    fix bug for responsive table

--- app/assets/javascripts/components/admin_search_results.js.jsx
+++ app/assets/javascripts/components/admin_search_results.js.jsx
@@ -27,11 +27,12 @@ let AdminSearchResults = React.createClass({
 		return this.props.students.map((student) => {
 			return (
 				<tr key={student.id}>
-					<td>{student.student_number}</td>
-					<td>{student.name}</td>					
+					<td>{student.student_number}</td>					
+					<td className="hide-for-medium"><a href="#" onClick={this.handleClickDetail} data-student-number={student.student_number}>{student.name}</a></td>	
+					<td className="show-for-medium">{student.name}</td>			
 					<td><a href={`tel:${student.phone}`} target="_blank">{student.phone}</a></td>
-					<td>{student.attendance_records[0].created_at}</td>
-					<td>
+					<td className="show-for-medium">{student.attendance_records[0] ? student.attendance_records[0].created_at : "なし"}</td>
+					<td className="show-for-medium">
 						<button className="button" onClick={this.handleClickDetail} data-student-number={student.student_number}>
 							詳細
 						</button>
@@ -58,8 +59,8 @@ let AdminSearchResults = React.createClass({
 								<th>生徒番号</th>
 								<th>名前</th>
 								<th>電話番号</th>
-								<th>最終出席日</th>
-								<th></th>
+								<th className="show-for-medium">最終出席日</th>
+								<th className="show-for-medium"></th>
 							</tr>
 						</thead>
 						<tbody>
