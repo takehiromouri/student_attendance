76c0693d11de496a15844519dea04827a991590d
Takehiro Mouri <takehiromouri@gmail.com>
2016-12-14 16:25:51 +0900

    add panel

--- app/assets/javascripts/components/admin_student.js.jsx
+++ app/assets/javascripts/components/admin_student.js.jsx
@@ -2,23 +2,36 @@ let AdminStudent = React.createClass({
 	render(){
 		return (
 			<div>
-				<h2>{this.props.student.name}</h2>
-				<p><strong>生徒番号</strong> {this.props.student.student_number}</p>
-				<p><i className="fi-telephone"></i> <a href={`tel:${this.props.student.phone}`} target="_blank">{this.props.student.phone}</a></p>
+				<div className="row">
+					<div className="medium-4 small-12 columns">
+						<div  style={{"backgroundColor": "white", "padding": "20px"}}>
+							<h2>{this.props.student.name}</h2>
+							<p><strong>生徒番号</strong> {this.props.student.student_number}</p>
+							<p><i className="fi-telephone"></i> <a href={`tel:${this.props.student.phone}`} target="_blank">{this.props.student.phone}</a></p>
 
-				<div className="button-group float-right">
-					<a href={`/admin/students/${this.props.student.id}/edit`}>
-						<button className="button warning">						
-							登録編集
-						</button>
-					</a>	
-					<a href={`/admin/students/${this.props.student.id}`} 
-							 data-method="delete" 
-							 data-confirm="本当に解除しますか？">				
-						<button className="button alert">						
-							登録解除						
-						</button>
-					</a>
+							<div className="button-group">
+								<a href={`/admin/students/${this.props.student.id}/edit`}>
+									<button className="button warning small">						
+										登録編集
+									</button>
+								</a>	
+								<a href={`/admin/students/${this.props.student.id}`} 
+										 data-method="delete" 
+										 data-confirm="本当に解除しますか？">				
+									<button className="button alert small">						
+										登録解除						
+									</button>
+								</a>
+							</div>
+						</div>						
+					</div>
+
+					<div className="medium-8 small-12 columns">
+						<div style={{"backgroundColor": "white", "padding": "20px"}}>
+							<h3>メモ</h3>
+						</div>
+					</div>
+
 				</div>
 
 				<hr/>
